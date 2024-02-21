8ad2a30285ffd6d2579476f15f2b729bf235202f
OlatunjiAbiodunEmmanuel <olatunjia889@gmail.com>
2024-02-21 14:23:58 +0100

    my first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import CustomButton from "./CustomButton";
+import Image from "next/image";
+import hero1 from "@/public/hero.png";
+
+export default function Hero() {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find, Book or Rent a car - quickly!</h1>
+        <p className="hero__subtitle">
+          streamline your car rental experince with our effortless booking
+          process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"/>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/NavBar.tsx
@@ -0,0 +1,24 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import CustomButton from './CustomButton'
+
+
+
+export default function NavBar() {
+  return (
+   <header className='w-full absolute z-10'>
+<nav className='max-w-[1440px] mx-auto flex justify-between items-center px-6 sm:px-16 py-4'>
+    <Link href='/' className='flex justify-center items-center'>
+        <Image src="/logo.svg" alt='logo'
+        width={118}
+        height={118}
+        className='object-contain'
+        />
+    </Link>
+
+</nav>
+   </header>
+  )
+}
