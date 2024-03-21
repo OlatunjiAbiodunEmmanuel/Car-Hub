2fd53cd526ccf5773664255d4db7142a8130d763
OlatunjiAbiodunEmmanuel <olatunjia889@gmail.com>
2024-03-21 20:55:40 +0100

    add modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -41,10 +41,12 @@ export default function CarDetails({isOpen, closeModal, car}: CarDetailsProps) {
           leaveTo='opacity-0 scale-95'
           >
             
-            <Dialog.Panel className="relative w-full max-w-lg maxh-[90vh] overflow-y-auto transform rounded-2xl bg-white">
+            <Dialog.Panel className="relative w-full max-w-lg maxh-[90vh] overflow-y-auto transform
+             rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
               <button
               type='button'
               onClick={closeModal}
+              className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
               >
                 <Image src='/close.svg'
                 alt='close'
@@ -55,9 +57,12 @@ export default function CarDetails({isOpen, closeModal, car}: CarDetailsProps) {
 
                 
               </button>
+              <div className='flex-1 flex flex-col gap-3'>
+                <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+
+                </div>
+              </div>
             </Dialog.Panel>
-
-
           </Transition.Child>
             </div>
           </div>
